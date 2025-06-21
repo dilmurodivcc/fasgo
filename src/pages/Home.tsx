bfcc81df2b5cdbdea701758cdc5ca3b992d08d16
dilmurodivcc <zufarovamohira32@gmail.com>
2025-06-21 14:15:39 +0500

    Header done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import PartOne from "../components/partOne/page";
 import PartTwo from "../components/partTwo/index";
+import Header from "../components/layout/Header";
 
 const Home = () => {
   return (
     <div>
+      <Header/>
       <PartOne />
       <PartTwo />
     </div>
