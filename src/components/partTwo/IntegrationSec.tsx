24ece366501862d354d60873a8551391ba34be1a
Baxtiyorovich04 <markpomidorchik@gmail.com>
2025-06-21 15:25:18 +0500

    intergation banners sec

--- src/components/partTwo/IntegrationSec.tsx
+++ src/components/partTwo/IntegrationSec.tsx
@@ -1,10 +1,29 @@
 const IntegrationSec = () => {
   return (
     <section className="IntegratSec container">
-        <h1>Biz bilan ishlash</h1>
-      <div className="IntegratSec_interbanner interagB">
-        <div className="interbanner_text">
-
+      <h1 className="HeroTitle">Biz bilan ishlash</h1>
+      <div className="IntegratBannersSec">
+        <div className="IntegratBannersSec_banner interagB">
+          <div className="interbanner_text">
+            <h3>Fasgo hamkori bo’lish</h3>
+            <p>Platformamizga qo‘shilib, minglab mijozlarga yetib boring.</p>
+            <button className="pm-button white">
+              <span>Hamkor bo’lish</span>
+            </button>
+          </div>
+          <img className="BannerImg" src="/public/icons/home.svg" alt="" />
+          <img className="bannerBgimg" src="/public/icons/banerBg.svg" alt="" />
+        </div>
+        <div className="IntegratBannersSec_banner interagB">
+          <div className="interbanner_text">
+            <h3>Fasgo hamkori bo’lish</h3>
+            <p>Platformamizga qo‘shilib, minglab mijozlarga yetib boring.</p>
+            <button className="pm-button white">
+              <span>Hamkor bo’lish</span>
+            </button>
+          </div>
+          <img className="BannerImg" src="/public/icons/delevery.svg" alt="" />
+          <img className="bannerBgimg" src="/public/icons/banerBg.svg" alt="" />
         </div>
       </div>
     </section>
