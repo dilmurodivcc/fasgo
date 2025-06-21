2f1f8e886d72dcea0743fe43aa15ef0628dae819
Baxtiyorovich04 <markpomidorchik@gmail.com>
2025-06-21 14:23:10 +0500

    questions sec

--- src/components/partTwo/QuestionsSec.tsx
+++ src/components/partTwo/QuestionsSec.tsx
@@ -6,7 +6,6 @@ const QuestionsSec = () => {
         <div className="QuestionsBox_question">
           <div className="QuestionsBox_question_imgBox">
             <img src="/public/icons/QuestionStar1.svg" alt="" />
-           
           </div>
 
           <div className="QuestionsBox_questions_text">
@@ -20,13 +19,52 @@ const QuestionsSec = () => {
         <div className="QuestionsBox_question">
           <div className="QuestionsBox_question_imgBox">
             <img src="/public/icons/QuestionStar2.svg" alt="" />
-        
           </div>
 
           <div className="QuestionsBox_questions_text">
             <h3>Yetkazib berish qancha vaqt oladi?</h3>
             <p>
-            Buyurtma joylashgan joyga va restoran bandligiga qarab, o‘rtacha 30-45 daqiqa ichida yetkaziladi.
+              Buyurtma joylashgan joyga va restoran bandligiga qarab, o‘rtacha
+              30-45 daqiqa ichida yetkaziladi.
+            </p>
+          </div>
+        </div>
+        <div className="QuestionsBox_question">
+          <div className="QuestionsBox_question_imgBox">
+            <img src="/public/icons/QuestionStar3.svg" alt="" />
+          </div>
+
+          <div className="QuestionsBox_questions_text">
+            <h3>To‘lovni qanday amalga oshiraman?</h3>
+            <p>
+              Siz to‘lovni istalgan bank karta bilan yoki naqd pulda, yetkazib
+              beruvchi orqali amalga oshirishingiz mumkin.
+            </p>
+          </div>
+        </div>
+        <div className="QuestionsBox_question">
+          <div className="QuestionsBox_question_imgBox">
+            <img src="/public/icons/QuestionStar4.svg" alt="" />
+          </div>
+
+          <div className="QuestionsBox_questions_text">
+            <h3>Taom yetib kelmasa yoki muammo bo‘lsa nima qilaman?</h3>
+            <p>
+              Mijozlar xizmatiga murojaat qiling – biz muammoni tezda hal
+              qilamiz yoki pulingizni qaytaramiz.
+            </p>
+          </div>
+        </div>
+        <div className="QuestionsBox_question">
+          <div className="QuestionsBox_question_imgBox">
+            <img src="/public/icons/QuestionStar5.svg" alt="" />
+          </div>
+
+          <div className="QuestionsBox_questions_text">
+            <h3>Qanday taomlar mavjud?</h3>
+            <p>
+              Fasgo’da milliy, fastfud, vegetarian, osiyo, yevropa
+              oshxonalaridan minglab taomlar bor – har kun yangi tanlov!
             </p>
           </div>
         </div>
